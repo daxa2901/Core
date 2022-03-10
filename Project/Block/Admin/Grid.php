729edf54da50245aa1ecc909c03181185e4894d9
daxa2901 <daxadh24@gmail.com>
2022-03-10 19:03:05 +0530

    admin-login change

--- Project/Block/Admin/Grid.php
+++ Project/Block/Admin/Grid.php
@@ -13,6 +13,5 @@ class Block_Admin_Grid extends Block_Core_Template
 		$adminTable = Ccc::getModel('Admin');
 		$query = "SELECT * FROM `Admin`";
 		return $adminTable-> fetchAll($query);
-		
 	}
 }
