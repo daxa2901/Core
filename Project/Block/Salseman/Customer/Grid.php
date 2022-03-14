f0adb4b979a500207db7fe78d3af5513db929d16
daxa2901 <daxadh24@gmail.com>
2022-03-14 18:14:29 +0530

    mix-updates changes

--- Project/Block/Salseman/Customer/Grid.php
+++ Project/Block/Salseman/Customer/Grid.php
@@ -3,50 +3,20 @@
 <?php 
 class Block_Salseman_Customer_Grid extends Block_Core_Template
 {
-	protected $pager = null;
 	public function __construct()
 	{
 		$this->setTemplate('view/salseman/customer/grid.php');
 	}
 
-	public function setPager($pager)
-	{
-		$this->pager = $pager;
-		return $this;
-	}
-
-	public function getPager()
-	{
-		if(!$this->pager)
-		{
-			$this->setPager(Ccc::getModel('Core_Pager'));
-		}
-		return $this->pager;
-	}
-
 	public function getCustomers()
 	{
-		$request = Ccc::getModel('Core_Request');
 		$id = $this->id;
-		$page = ($page = (int)$request->getRequest('p',1))? $page : 1;
-		$pageCount = ($pageCount = (int)$request->getRequest('ppc',10)) ?$pageCount : 10;
-		$query = "SELECT count(`customerId`) FROM `customer` WHERE `salsemanId` = {$id} OR `salsemanId` IS NULL";
-		$totalCount = $this->getAdapter()->fetchOne($query);
-		$this->getPager()->execute($totalCount,$page,$pageCount);
-		$startLimit = $this->getPager()->getStartLimit()-1;
 		$customers = Ccc::getModel('Customer');
 		$query = "SELECT 
 					* 
 					FROM `customer` 
-				WHERE `salsemanId` = {$id} OR `salsemanId` IS NULL LIMIT {$startLimit} , {$this->getPager()->getPerPageCount()}";
+				WHERE `salsemanId` = {$id} OR `salsemanId` IS NULL";
 		$customers =  $customers->fetchAll($query);
-		if(!$customers)
-		{
-			$action = new Controller_Core_Action();
-			$this->getPager()->setCurrent(($this->getPager()->getCurrent() == 1) ? 1 :$action->redirect(null,null,['p'=>$this->getPager()->getCurrent()-1]));
-		}
 		return $customers;
-			
-
 	}
 }
--- Project/view/salseman/customer/grid.php
+++ Project/view/salseman/customer/grid.php
@@ -2,54 +2,10 @@
 
 <div class='container' style="text-align: center; ">
 <h1> Salseman Customer Details </h1> 
-<script type="text/javascript">
-	function changeURL() 
-	{
-		const pprValue = document.getElementById('ppc').selectedOptions[0].value;
-		let href = window.location.href;
-		if(!href.includes('ppc'))
-		{
-		  	href+='&ppc=20';
-		}
-		const myArray = href.split("&");
-		for (i = 0; i < myArray.length; i++)
-		{
-			if(myArray[i].includes('p='))
-			{
-			  	myArray[i]='p=1';
-			}
-			if(myArray[i].includes('ppc='))
-			{
-			  	myArray[i]='ppc='+pprValue;
-			}
-		}
-			const str = myArray.join("&");
-			location.replace(str);
-	}
-
-	</script>
-	<table  align="center" cellspacing="20">
-		<tr>
-			<td> 
-				<select name="perPageCountOption" onchange="changeURL()" id='ppc'>
-						<option value="">select Per Page Count Option</option>
-					<?php foreach ($this->getPager()->getPerPageCountOption() as $key => $value) : ?>
-						<option value="<?php echo $value ?>"> <?php echo $value ?></option>
-					<?php endforeach; ?>
-				</select>
-			</td>
-			<td><a href="<?php echo $this->getUrl(null,null,['p'=>$this->getPager()->getStart()]) ?>" <?php if(!$this->getPager()->getStart()): ?> style = "pointer-events : none;" <?php endif; ?>>Start</a></td>
-			<td><a href="<?php echo $this->getUrl(null,null,['p'=>$this->getPager()->getPrev()]) ?>"<?php if(!$this->getPager()->getPrev()): ?> style = "pointer-events : none;"<?php endif; ?>>Previous</a></td>
-			<td><a href="<?php echo $this->getUrl(null,null,['p'=>$this->getPager()->getCurrent()]) ?>">Current</a></td>
-			<td><a href="<?php echo $this->getUrl(null,null,['p'=>$this->getPager()->getNext()]) ?>" <?php if(!$this->getPager()->getNext()): ?> style = "pointer-events : none;" <?php endif; ?>>Next</a></td>
-			<td><a href="<?php echo $this->getUrl(null,null,['p'=>$this->getPager()->getEnd()]) ?>" <?php if(!$this->getPager()->getEnd()): ?> style = "pointer-events : none;" <?php endif; ?>>End</a></td>
-		</tr>
-	</table>	
-
 <form action=<?php echo $this->getUrl('save');?> method="POST">
 	<button type="submit" class="Registerbtn"> Update </button>
 
-	<a href="<?php echo $this->getUrl('grid','salseman',['id'=>null,'p'=>1]);?>"><button type="button" class="cancel">Cancel</button></a>
+	<a href="<?php echo $this->getUrl('grid','salseman',['id'=>null]);?>"><button type="button" class="cancel">Cancel</button></a>
 		
 
 	<div id='info'>
